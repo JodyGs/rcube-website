a408943cae7b882ecc739253cc28cd251a2aabe1
Jody Gs <jodygs@Jodys-MacBook-Pro.local>
2024-05-05 18:01:05 +0200

    footer resp.

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -7,9 +7,9 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <>
-      <div className="col-span-5 col-start-2 space-y-8 py-20 text-custom-grey-300">
-        <RcubeLogo className="w-[144px] " />
-        <h3 className="min-h-[171px] max-w-[344px] leading-6">
+      <div className="col-span-full space-y-8 px-2 py-10 text-custom-grey-300 md:px-12 lg:col-span-5 lg:col-start-2 lg:px-0 lg:py-20">
+        <RcubeLogo className="w-[144px]" />
+        <h3 className="max-w-[344px] leading-6 lg:min-h-[171px]">
           The  360° platform for asset management entrepreneurs.
         </h3>
         <div>
@@ -28,13 +28,15 @@ export default function Footer() {
           </div>
         </div>
       </div>
-      <div className="col-span-6 border-x-[1px] border-custom-grey-400 pb-[120px]">
-        <h4 className="font-satoshi px-12 py-20 text-3xl">Legal</h4>
+      <div className="col-span-full border-t-[1px] border-custom-grey-400 lg:col-span-6 lg:border-x-[1px] lg:pb-[120px]">
+        <h4 className="px-2 py-10 font-satoshi text-3xl md:px-12 lg:py-20">
+          Legal
+        </h4>
         <div className="space-y-4 text-custom-grey-100">
           <ul>
             {NAVIGATION.legal.map(({ name, href }) => (
               <li
-                className="border-b-[1px] border-custom-grey-400 px-12 py-4 first:border-t-[1px]"
+                className="border-b-[1px] border-custom-grey-400 px-2 py-4 first:border-t-[1px] md:px-12"
                 key={name}
               >
                 <Link href={href}>{name}</Link>
@@ -43,13 +45,15 @@ export default function Footer() {
           </ul>
         </div>
       </div>
-      <div className="col-span-6 pb-[120px]">
-        <h4 className="font-satoshi px-12 py-20 text-3xl">Social</h4>
+      <div className="col-span-full pb-10 lg:col-span-6 lg:border-x-[1px] lg:pb-[120px]">
+        <h4 className="px-2 py-10 font-satoshi text-3xl md:px-12 lg:py-20">
+          Social
+        </h4>
         <div className="space-y-4 text-custom-grey-100">
           <ul>
             {NAVIGATION.social.map(({ name, href }) => (
               <li
-                className="border-b-[1px] border-custom-grey-400 px-12 py-4 first:border-t-[1px]"
+                className="border-b-[1px] border-custom-grey-400 px-2 py-4 first:border-t-[1px] md:px-12"
                 key={name}
               >
                 <Link href={href}>{name}</Link>
